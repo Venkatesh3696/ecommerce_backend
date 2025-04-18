39ee9cd1d8d9aed513a8877e65aa71b76e59fd91
Venkatesh3696 <venkateshpentakota888@gmail.com>
2025-04-18 22:04:33 +0530

    add mongouri

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/api/shopping/products", shopProductsRouter);
 app.use("/api/shopping/cart", checkUser, cartRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!", req.ip);
+  res.status(200).send("Hello World!", req.ip);
 });
 
 const PORT = process.env.PORT || 5000;
